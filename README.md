e2854933971ea31e40bf1ff370bb24ae8ae4144d
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2024-07-09 14:35:57 +0800

    First push
    Added simple routes to "/" for http methods.

---
+++ README.md
@@ -0,0 +1,9 @@
+# top_API_restapi-tutorial
+
+This is the exercise from [API Basics | The Odin Project](https://www.theodinproject.com/lessons/nodejs-api-basics).
+
+This code was created by following along this guide: [How to create a REST API with Express.js in Node.js](https://www.robinwieruch.de/node-express-server-rest-api/).
+
+Overview of steps
+1. Added simple endpoints on home page to demonstrate use of http methods for API responses.
+2. 
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// Set up simple routes for API tutorial
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
+module.exports = app;
