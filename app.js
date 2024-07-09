2696025aea4a4e78a5bd9b207870bd7e6ff0d4c5
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2024-07-09 22:04:01 +0800

    Refactor to use models directory
    Model refactoring so that sample user and message data now found in models/index.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const { v4: uuidv4 } = require('uuid');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+const models = require('./models');
+
 const app = express();
 
 app.use(logger('dev'));
@@ -18,8 +20,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    req.me = users[1];
-    next();
+    // context object is a container for everything that is passed to the routes. Not necessary, but good practice.
+    req.context = {
+        models,
+        me: models.users[1],
+    };
 })
 
 // app.use('/', indexRouter);
@@ -27,12 +32,16 @@ app.use((req, res, next) => {
 
 // Set up simple routes for API tutorial
 
+app.get('/session', (req, res) => {
+    return res.send(req.context.models.users[req.context.me.id]);
+});
+
 app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
+    return res.send(Object.values(req.context.models.users));
 });
 
 app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
+    return res.send(req.context.models.users[req.params.userId]);
 });
 
 app.post('/users', (req, res) => {
@@ -52,11 +61,11 @@ app.delete('/users/:userId', (req, res) => {
 });
 
 app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages));
+    return res.send(Object.values(req.context.models.messages));
 });
 
 app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
+    return res.send(req.context.models.messages[req.params.messageId]);
 });
 
 app.post('/messages', (req, res) => {
@@ -64,10 +73,10 @@ app.post('/messages', (req, res) => {
     const message = {
         id,
         text: req.body.text,
-        userId: req.me.id,
+        userId: req.context.me.id,
     };
 
-    messages[id] = message;
+    req.context.models.messages[id] = message;
 
     return res.send(message);
 });
@@ -76,43 +85,11 @@ app.delete('/messages/:messageId', (req, res) => {
     const {
         [req.params.messageId]: message,
         ...otherMessages
-    } = messages;
+    } = req.context.models.messages;
 
-    messages = otherMessages;
+    req.context.models.messages = otherMessages;
 
     return res.send(message);
 });
-
-app.get('/session', (req, res) => {
-    return res.send(users[req.me.id]);
-});
-
-
-// sample data to test API's
-
-let users = {
-    1: {
-        id: '1',
-        username: 'Robin Wieruch',
-    },
-    2: {
-        id: '2',
-        username: 'Dave Davids',
-    },
-};
-
-let messages = {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'By World',
-        userId: '2',
-    },
-};
-
 
 module.exports = app;
